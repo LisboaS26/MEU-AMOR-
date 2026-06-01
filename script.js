1d9e888b1fcc988ac4a6eaa15d767002d3c2c58d
LisboaS26 <pauloantoniolisboaaraujo71@gmail.com>
2026-06-01 20:53:59 -0300

    Update script.js

--- script.js
+++ script.js
@@ -16,7 +16,7 @@ function closePopup(){
 
 /* DATA DO RELACIONAMENTO */
 
-const startDate = new Date("2024-01-01T00:00:00");
+const startDate = new Date("2024-06-10T00:00:00");
 
 function updateCounter(){
 
