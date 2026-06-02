abea31591d1c9ae25cb3e1cf68758b4f8aaef783
LisboaS26 <pauloantoniolisboaaraujo71@gmail.com>
2026-06-02 19:39:59 -0300

    Update script.js

--- script.js
+++ script.js
@@ -84,3 +84,20 @@ for (let i = 0; i < 80; i++) {
 }
   
 }
+
+const starsContainer = document.querySelector(".stars");
+
+function createStars() {
+  for (let i = 0; i < 80; i++) {
+    const star = document.createElement("div");
+    star.classList.add("star");
+
+    star.style.left = Math.random() * 100 + "vw";
+    star.style.top = Math.random() * 100 + "vh";
+
+    star.style.animationDuration = (3 + Math.random() * 5) + "s";
+    star.style.opacity = Math.random();
+
+    starsContainer.appendChild(star);
+  }
+}
