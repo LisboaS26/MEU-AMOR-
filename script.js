07836595f88b4ab60cae46e04fc20c713d7b086c
LisboaS26 <pauloantoniolisboaaraujo71@gmail.com>
2026-06-02 21:46:01 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,10 @@
 console.log("JS carregou");
 
+console.log("script carregou");
+window.toggleMusic = function(){
+  console.log("toggle funcionando");
+};
+
 console.log("btn:", document.getElementById("musicBtn"));
 console.log("music:", document.getElementById("ourMusic"));
 
