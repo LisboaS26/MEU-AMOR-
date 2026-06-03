65c45183717881d068588418582293cd29d9df6a
LisboaS26 <pauloantoniolisboaaraujo71@gmail.com>
2026-06-02 21:32:30 -0300

    Update script.js

--- script.js
+++ script.js
@@ -21,6 +21,9 @@ function closePopup(){
 
 /* DATA DO RELACIONAMENTO */
 
+const progressBar = document.getElementById("progressBar");
+const music = document.getElementById("ourMusic");
+
 const startDate = new Date("2024-06-10T00:00:00");
 
 function updateCounter(){
